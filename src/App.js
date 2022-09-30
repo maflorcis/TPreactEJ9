b5c874aac1e2d507cc9c8d48e04363d2b191b211
floralonso <floralonso@gmail.com>
2022-09-30 08:31:31 -0300

    análisis ejercicio 9 según investigado en la web

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/header';
+import NuevaCita from './components/nuevaCita';
+import ListaCitas from './components/listaCitas';
+
+class App extends Component {
+  state ={
+    citas: []    
+
+  }
+
+  //CUANDO LA APLICACION CARGA
+    componentDidMount(){
+      const citasLS = localStorage.getItem('citas');
+      if(citasLS){
+          this.setState({
+            citas : JSON.parse(citasLS)
+          })
+      }
+    }
+
+  // CUANDO ELEMINAMOS O AGREGAMOS UNA NUEVA CITA
+
+  componentDidUpdate(){
+    localStorage.setItem('citas', JSON.stringify(this.state.citas))
+  }
+
+  crearNuevaCita = datos => {
+    //COPIAR EL STATE ACTUAL
+    const citas = [...this.state.citas, datos]
+
+    // AGREGAR EL NUEVO STATE
+
+    this.setState({
+      citas: citas
+    })
+  }
+
+    //ELIMINA LAS CITAS DEL STATE
+      eliminarCita = id =>{
+        //HACER UNA COPIA DEL STATE
+          const citasActuales = [...this.state.citas]
+
+        //UTILIZAR FILTER PARA SACAR EL ELEMENTO ID DEL ARRAY
+        const citas = citasActuales.filter(cita => cita.id !== id);
+
+        //ACTUALIZAR EL STATE
+        this.setState({
+          citas
+        })
+
+      }
+
+  render(){
+    return (
+      <div className="container">
+        <Header
+        titulo='Administrador Pacientes Veterinario'
+        />
+        <div className="row">
+          <div className="col-md-10 mx-auto">
+            <NuevaCita
+            crearNuevaCita ={this.crearNuevaCita}
+            />
+          </div>
+
+            <div className="mt-5 col-md-10 mx-auto">
+            <ListaCitas
+              citas={this.state.citas}
+              eliminarCita={this.eliminarCita}
+              />
+            </div>
+        </div>
+      </div>   
+    );
+  }
 }
 
 export default App;
